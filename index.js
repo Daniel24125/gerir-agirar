23865a4129799b8dfafe7f2735aeb9f2ef2817f3
Daniel24125 <danielmadalena@msn.com>
2020-06-30 11:14:33 +0100

    CORS

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'http://agirar.pt');
-  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://agirar.pt');
+
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
